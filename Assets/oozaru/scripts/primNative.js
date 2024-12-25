64f5cee1cca2108d40527dce015a1fe83ea6d341
XM <xm@alias.black>
2024-12-25 21:02:07 +0800

    Improved PrimNative implementation for rounded rectangles

--- Assets/oozaru/scripts/primNative.js
+++ Assets/oozaru/scripts/primNative.js
@@ -45,53 +45,52 @@ class PrimNative
 	// Draw Line Rounded Rectangle.
 	static drawRoundedRectangle(surface, x1, y1, x2, y2, rx, ry, thickness, color)
 	{
-		PrimNative.drawArcInternal(surface, x1 + rx, y1 + ry, rx, ry, Math.PI * .5, Math.PI, thickness, color)
-		PrimNative.drawArcInternal(surface, x2 - rx, y1 + ry, rx, ry, 0, Math.PI / 2, thickness, color)
-		PrimNative.drawArcInternal(surface, x1 + rx, y2 - ry, rx, ry, Math.PI, Math.PI * 1.5, thickness, color)
-		PrimNative.drawArcInternal(surface, x2 - rx, y2 - ry, rx, ry, -Math.PI / 2, 0, thickness, color)
-		PrimNative.drawLine(surface, x1 + rx, y1 + thickness / 2, x2 - rx, y1 + thickness / 2, thickness, color)
-		PrimNative.drawLine(surface, x2 - thickness / 2, y1 + ry, x2 - thickness / 2, y2 - ry, thickness, color)
-		PrimNative.drawLine(surface, x1 + rx, y2 - thickness / 2, x2 - rx, y2 - thickness / 2, thickness, color)
-		PrimNative.drawLine(surface, x1 + thickness / 2, y1 + ry, x1 + thickness / 2, y2 - ry, thickness, color)
+		let vertices = [
+			...this.getArcCoords(x2 - rx, y2 - ry, rx, ry, -Math.PI / 2, 0, thickness, color),
+			...this.getArcCoords(x1 + rx, y1 + ry, rx, ry, Math.PI * .5, Math.PI, thickness, color),
+			...this.getArcCoords(x1 + rx, y2 - ry, rx, ry, Math.PI, Math.PI * 1.5, thickness, color),
+			...this.getArcCoords(x2 - rx, y1 + ry, rx, ry, 0, Math.PI / 2, thickness, color),
+			...this.getArcCoords(x2 - rx, y2 - ry, rx, ry, -Math.PI / 2, 0, thickness, color).slice(0, 2)
+		]
+		Shape.drawImmediate(surface, ShapeType.TriStrip, vertices)
 	}
 
 	// Draw Filled Rounded Rectangle.
 	static drawFilledRoundedRectangle(surface, x1, y1, x2, y2, rx, ry, color)
 	{
-		PrimNative.drawPieInternal(surface, x1 + rx, y1 + ry, rx, ry, Math.PI * .5, Math.PI, color)
-		PrimNative.drawPieInternal(surface, x2 - rx, y1 + ry, rx, ry, 0, Math.PI / 2, color)
-		PrimNative.drawPieInternal(surface, x1 + rx, y2 - ry, rx, ry, Math.PI, Math.PI * 1.5, color)
-		PrimNative.drawPieInternal(surface, x2 - rx, y2 - ry, rx, ry, -Math.PI / 2, 0, color)
-		PrimNative.drawFilledRectangle(surface, x1 + rx - 1, y1, x2 - rx + 1, y2, color)
-		PrimNative.drawFilledRectangle(surface, x1, y1 + ry - 1, x1 + rx, y2 - ry + 1, color)
-		PrimNative.drawFilledRectangle(surface, x2 - rx, y1 + ry - 1, x2, y2 - ry + 1, color)
+		let vertices = [
+			{ x: (x1 + x2) / 2, y: (y1 + y2) / 2, color: color },
+			...this.getArcCoords(x2 - rx, y1 + ry, rx, ry, 0, Math.PI / 2, 0, color),
+			...this.getArcCoords(x1 + rx, y1 + ry, rx, ry, Math.PI * .5, Math.PI, 0, color),
+			...this.getArcCoords(x1 + rx, y2 - ry, rx, ry, Math.PI, Math.PI * 1.5, 0, color),
+			...this.getArcCoords(x2 - rx, y2 - ry, rx, ry, -Math.PI / 2, 0, 0, color),
+			this.getArcCoords(x2 - rx, y1 + ry, rx, ry, 0, Math.PI / 2, 0, color)[0]
+		]
+		Shape.drawImmediate(surface, ShapeType.Fan, vertices)
 	}
 
 	// Internal draw arc with different X and Y radii.
 	static drawArcInternal(surface, x, y, rx, ry, angle1, angle2, thickness, color)
 	{
-		let numSegments = Math.ceil(10 * Math.sqrt((rx + ry) / 2.0))
-		let increment = Math.PI * 2 / numSegments
-		let vertices = []
-		for (let i = angle1; i < angle2; i += increment)
-		{
-			vertices.push({ x: x + Math.cos(i) * rx, y: y - Math.sin(i) * ry, color: color })
-			vertices.push({ x: x + Math.cos(i) * (rx - thickness), y: y - Math.sin(i) * (ry - thickness), color: color })
-		}
-		vertices.push({ x: x + Math.cos(angle2) * rx, y: y - Math.sin(angle2) * ry, color: color })
-		vertices.push({ x: x + Math.cos(angle2) * (rx - thickness), y: y - Math.sin(angle2) * (ry - thickness), color: color })
+		let vertices = this.getArcCoords(x, y, rx, ry, angle1, angle2, thickness, color)
 		Shape.drawImmediate(surface, ShapeType.TriStrip, vertices)
 	}
 
-	// Internal draw pie shape.
-	static drawPieInternal(surface, x, y, rx, ry, angle1, angle2, color)
+	static getArcCoords(x, y, rx, ry, angle1, angle2, thickness, color)
 	{
 		let numSegments = Math.ceil(10 * Math.sqrt((rx + ry) / 2.0))
 		let increment = Math.PI * 2 / numSegments
-		let vertices = [{ x: x, y: y, color: color }]
+		let vertices = []
+
 		for (let i = angle1; i < angle2; i += increment)
+		{
 			vertices.push({ x: x + Math.cos(i) * rx, y: y - Math.sin(i) * ry, color: color })
-		Shape.drawImmediate(surface, ShapeType.Fan, vertices)
+			if (thickness > 0) vertices.push({ x: x + Math.cos(angle2) * (rx - thickness), y: y - Math.sin(angle2) * (ry - thickness), color: color })
+		}
+		vertices.push({ x: x + Math.cos(angle2) * rx, y: y - Math.sin(angle2) * ry, color: color })
+		if (thickness > 0) vertices.push({ x: x + Math.cos(angle2) * (rx - thickness), y: y - Math.sin(angle2) * (ry - thickness), color: color })
+		
+		return vertices
 	}
 
 	// Ignore for now.
